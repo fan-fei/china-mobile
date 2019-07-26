83888ebb331b6ea5b4d14165dc1ec2d8522cb695
樊飞 <fanfei@ctv.org.cn>
2019-07-26 14:34:24 +0800

    update

--- china-mobile/src/main/java/shop/ShopService.java
+++ china-mobile/src/main/java/shop/ShopService.java
@@ -8,10 +8,12 @@ import org.springframework.util.DigestUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.extern.slf4j.Slf4j;
 import shop.bean.BaseResult;
 import shop.bean.ErrorCode;
 import shop.bean.NotifyOrderReq;
 
+@Slf4j
 @Service
 public class ShopService {
 
@@ -24,12 +26,16 @@ public class ShopService {
         String request = new String(Base64.getDecoder().decode(req));
         String inputSign = request.substring(0, 32);
         String inputJson = request.substring(32);
+        log.info(secretKey);
+        log.info(inputSign);
+        log.info(inputJson);
 
         NotifyOrderReq notifyOrderReq = new ObjectMapper().readValue(inputJson, NotifyOrderReq.class);
 
         char[] requestJsonChars = inputJson.toCharArray();
         Arrays.sort(requestJsonChars);
         String md5Sign = DigestUtils.md5DigestAsHex(secretKey.concat(String.valueOf(requestJsonChars)).getBytes()).toLowerCase();
+        log.info(md5Sign);
         if (!inputSign.equals(md5Sign)) {
             baseResult.setErrorCodeDef(ErrorCode.FAIL_SIGN_WRONG);
             return baseResult;
